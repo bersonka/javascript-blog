9556ab036adb3874e7912fb55f76a141e0f1660b
Olga Bera <olga.bera@gmail.com>
2022-02-14 19:19:41 +0100

    add active to clicked links

--- js/script.js
+++ js/script.js
@@ -5,20 +5,22 @@
 
 
   const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
+    activeLink.classList.add('active');
     }   
   
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    console.log('clickedElement (with plus): ' + clickedElement('.titles a.active'));
+    clickedElement.classList.add('active');
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.posts a.active');
 
