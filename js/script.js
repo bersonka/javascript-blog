aa110142cc7dc885a429d2380898413f1f4f51fa
Olga Bera <olga.bera@gmail.com>
2022-02-14 21:54:12 +0100

    adding class 'active' to correct article

--- js/script.js
+++ js/script.js
@@ -36,11 +36,16 @@
     const articleSelector = clickedElement.getAttribute("href");
     console.log(articleSelector);
 
-    /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
     console.log(targetArticle);
 
-    /* add class 'active' to the correct article */
+    /* [DONE] add class 'active' to the correct article */
+    console.log('clickedElement (with plus): ' + targetArticle);
+    targetArticle.classList.add('active');
+
+
+
   }
   
   const links = document.querySelectorAll('.titles a');
