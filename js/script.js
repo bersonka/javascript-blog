b2af64de37d0ca5ec7fcc399e6eef57b0b5330b6
Olga Bera <olga.bera@gmail.com>
2022-02-23 21:35:18 +0100

    add function generateTags- in progress, finding all articles, start creating loop

--- js/script.js
+++ js/script.js
@@ -90,3 +90,38 @@
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+  function generateTags(){
+    /* find all articles */
+    optArticleTagsSelector = '.post-tags .list';
+    const titleList = document.querySelector(optTitleListSelector);
+  
+    /* START LOOP: for every article: */
+    for(let article of articles){
+
+      /* find tags wrapper */
+      const tagsWrapper = article.querySelector(optArticleTagsSelector);
+
+      /* make html variable with empty string */
+      let html = '';
+
+      /* get tags from data-tags attribute */
+      const articleTags = clickedElement.getAttribute('data-tags');
+  
+      /* split tags into array */
+  
+      /* START LOOP: for each tag */
+  
+        /* generate HTML of the link */
+  
+        /* add generated code to html variable */
+  
+      /* END LOOP: for each tag */
+  
+      /* insert HTML of all the links into the tags wrapper */
+  
+    /* END LOOP: for every article: */
+    }
+  }
+  
+  generateTags();
